9072b0a4cea6c32ac81057c1ae948196d56fb49b
Hemant Mishra <hemantmishra1452968@gmail.com>
2025-09-21 22:34:09 +0530

    Add files via upload

---
+++ LeapYear.cpp
@@ -0,0 +1,12 @@
+// User function Template for C
+
+int checkYear(int N) {
+    if(N%400 == 0 )
+    return 1;
+    else if (N%4 ==0 && N%100!=0)
+    return 1;
+    else
+    return 0;
+    
+    
+}
